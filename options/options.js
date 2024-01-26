60ca609e0e3b270ed018dd25a06d8dcb714d263c
Crowdbotics <team@crowdbotics.com>
2024-01-26 20:33:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ameris-moneymingle-45604",
+  url: "https://ameris-moneymingle-45604.botics.co",
+  api: "https://ameris-moneymingle-45604.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
